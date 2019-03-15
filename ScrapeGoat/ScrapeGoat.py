2e233801b97fdab6ecb20b685cc3824958cbb92f
azorlu <mandelbug@gmail.com>
2019-03-15 09:38:32 +0900

    Enabled saving data as Json

--- ScrapeGoat/ScrapeGoat.py
+++ ScrapeGoat/ScrapeGoat.py
@@ -1,6 +1,11 @@
 ﻿import requests
 from bs4 import BeautifulSoup
+import json
 import numpy as np
+
+# CONSTANTS
+BASE_URL = "https://en.wikipedia.org"
+CATEGORY_TITLE = "Category:19th-century_classical_composers"
 
 def getPageContent(pageUrl):
     page = requests.get(pageUrl)
@@ -21,16 +26,28 @@ def getLinks(links):
         linksDict[a['title']] = a['href']
     return linksDict
 
-    
+def writeToFile(obj, filename):
+    with open(filename, 'w') as fp:
+        json.dump(obj, fp)
+
+def getCategoryPages():
+    pageUrl = BASE_URL + '/wiki/' + CATEGORY_TITLE
+    selector = 'div.mw-category-group a'
+    content = getPageContent(pageUrl)
+    print(content)
  
-# 
-pageUrl = 'https://en.wikipedia.org/wiki/Category:19th-century_classical_composers'
-selector = 'div.mw-category-group a'
-content = getPageContent(pageUrl)
 
-if (content != None):
-    print(getLinks(getSelection(content,selector)))
+def createList():
+    pageUrl = BASE_URL + '/wiki/' + CATEGORY_TITLE
+    selector = 'div.mw-category-group a'
+    content = getPageContent(pageUrl)
+    filename = 'data/composers.json'
+        
+    if (content != None):
+        links = getLinks(getSelection(content,selector))
+        writeToFile(links, filename)
 
-
+# action
+# createList()
 
 
